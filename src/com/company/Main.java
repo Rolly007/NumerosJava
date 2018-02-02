377a1445b1ee06cc6af87f7978fdba75eaf20ee8
Americo <r.americo.quiroz@gmail.com>
2018-02-01 21:01:25 -0400

    Agregando el metodo esprimo()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        int n=0;
         //leer datos del teclado para que el usuario introduzca
         Scanner scanner=new Scanner(System.in);
-        String valor=scanner.nextLine();
+            do {
+                System.out.println("Introduzca un numero y cero para salir");
+                //Almacenamos en una variable de tipo cadena.
+                String valor = scanner.nextLine();
 
-        //ahora convertimos una cadena a entero
-        int n=Integer.parseInt(valor);
+                //ahora convertimos una cadena a entero
+                n = Integer.parseInt(valor);
 
-        NumeroJava num= new NumeroJava(n);
-        System.out.println(num.esPar());
+                NumeroJava num = new NumeroJava(n);
+                System.out.println(num.esPar());
 
-        if(num.esPar()){
-            System.out.print("El numero ingresado es par!");
-        }
-        else System.out.print("El numero ingresado es impar!!");
+                if (num.esPar()) {
+                    System.out.println("El numero ingresado es par!");
+                } else System.out.println("El numero ingresado es impar!!");
 
+                if (num.esprimo() == 1) {
+                    System.out.println("El numero es primo");
+                } else System.out.println("El numero no es Primo");
+            }while(n!=0);
     }
 }
--- src/com/company/NumeroJava.java
+++ src/com/company/NumeroJava.java
@@ -9,7 +9,13 @@ public class NumeroJava {
         return num1%2==0; //aqui devuelve el resultado de evaluar la expresion
     }
 
-    /*public NumeroJava esprimo(){
-
-    }*/
+    public int esprimo(){
+        int suma=0;
+        for(int i=1; i<=num1-1;i++){
+            if(num1%i==0) {
+                suma = suma + 1;
+            }
+        }
+        return suma;
+    }
 }
